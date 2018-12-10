7a0073d49c37960afe5534aeff975c67703eb959
Laimison <laimison1@gmail.com>
2018-12-10 09:29:40 +0000

    created some_incoming_queue

--- make_binding.py
+++ make_binding.py
@@ -10,18 +10,17 @@ rabbitmq_rcv_exchange = "some_exchange"
 rabbitmq_rcv_queue = "some_incoming_queue"
 rabbitmq_rcv_key = "some_routing_key"
 
-rabbitmq_user = "testuser"
-rabbitmq_password = "testpassword"
+# rabbitmq_user = "testuser"
+# rabbitmq_password = "testpassword"
 
-rabbitmq_user_guest = "guest"
-rabbitmq_password_guest = "guest"
+rabbitmq_user = "guest"
+rabbitmq_password = "guest"
 
 outgoingRoutingKeys = ["outgoing_routing_key"]
-outgoingQueues = ["some_outgoing_queue "]
+outgoingQueues = ["some_outgoing_queue"]
 
 # The binding area
-# credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
-credentials = pika.PlainCredentials(rabbitmq_user_guest, rabbitmq_password_guest)
+credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
 connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host, rabbitmq_port, rabbitmq_virtual_host, credentials))
 channel = connection.channel()
 channel.queue_bind(exchange=rabbitmq_rcv_exchange, queue=rabbitmq_rcv_queue, routing_key=rabbitmq_rcv_key)
--- make_queue.sh
+++ make_queue.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 rabbitmqadmin declare queue --vhost=Some_Virtual_Host name=some_outgoing_queue durable=true || exit 1
+rabbitmqadmin declare queue --vhost=Some_Virtual_Host name=some_incoming_queue || exit 1
 echo success
