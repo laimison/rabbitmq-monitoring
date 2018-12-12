58e8d1b0a72b4513fddff8031900a43b7db61d24
Laimison <laimison1@gmail.com>
2018-12-12 17:35:42 +0000

    mon changed to monitoring user

--- add/add_user_monitoring.sh
+++ add/add_user_monitoring.sh
@@ -2,5 +2,5 @@
 
 rabbitmqctl add_user monitoring password || exit 1
 rabbitmqctl set_user_tags monitoring monitoring || exit 1
-rabbitmqctl set_permissions -p / mon "" "" "" || exit 1
+rabbitmqctl set_permissions -p / monitoring "" "" "" || exit 1
 echo success
