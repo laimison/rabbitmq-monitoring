6923e7296bf55f4ee189374d1aee6b27435b76c3
Laimison <laimison1@gmail.com>
2018-12-19 12:02:22 +0000

    rest_query function to do GET and output results & some initial json parsing added to play with

--- monitoring.go
+++ monitoring.go
@@ -7,31 +7,69 @@ import (
     "io/ioutil"
     "net/http"
     "log"
+    "encoding/json"
+    "time"
     // "bytes"
-    // "encoding/json"
     // "os"
 )
 
 func rest_query(method string, address string, user string, pass string) string {
-  client := &http.Client{}
+  // HTTP client to connect to API
+  // client := &http.Client{}
+  var client_timeout = &http.Client{
+    Timeout: time.Second * 10,
+  }
 
+  // Doing query
   request, error := http.NewRequest(method, address, nil)
 
+  // Basic HTTP authentication
   request.SetBasicAuth(user, pass)
 
-  response, error := client.Do(request)
+  // Getting the output
+  response, error := client_timeout.Do(request)
 
+  // Throw error
   if error != nil{
       log.Fatal(error)
   }
 
+  // Write the output to memory
   bodyText, error := ioutil.ReadAll(response.Body)
-  content := string(bodyText)
 
+  // Getting the output
+  var result map[string]interface{}
+  json.Unmarshal([]byte(bodyText), &result)
+
+  // fmt.Println(result)
+  content := string(bodyText)
+  // content := string(result)
   return content
 }
 
 func main() {
-  content := rest_query("GET", "http://localhost:15672/api/queues", "monitoring", "password")
-  fmt.Println(content)
+  // Doing HTTP query
+  rest_query_content := rest_query("GET", "http://localhost:15672/api/queues", "monitoring", "password")
+  fmt.Println(rest_query_content)
+
+  // Parsing json
+  type Bird struct {
+    Species string
+    Description string
+  }
+
+  birdJson := `{"species": "pigeon","description": "likes to perch on rocks"}`
+  var bird Bird
+
+  json.Unmarshal([]byte(birdJson), &bird)
+
+  fmt.Printf("\nSpecies:\n%s\n\nDescription:\n%s\n", bird.Species, bird.Description)
+
+  // Converting string to json
+  jsonString := "{\"foo\":{\"baz\": [1,2,3]}}"
+
+  var jsonMap map[string]interface{}
+  json.Unmarshal([]byte(jsonString ), &jsonMap)
+
+  fmt.Println(jsonMap) 
 }
