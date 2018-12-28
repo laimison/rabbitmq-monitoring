f0b7e2d30364f76c8965095a7b98fcef7a02c584
Laimison <laimison1@gmail.com>
2018-12-28 17:16:21 +0000

    initial queues monitoring script

--- add/add_queues.sh
+++ add/add_queues.sh
@@ -2,4 +2,6 @@
 
 rabbitmqadmin declare queue --vhost=Some_Virtual_Host name=some_outgoing_queue durable=true || exit 1
 rabbitmqadmin declare queue --vhost=Some_Virtual_Host name=some_incoming_queue || exit 1
+rabbitmqadmin declare queue --vhost=Some_Virtual_Host name=ignore_this_queue || exit 1
+rabbitmqadmin declare queue --vhost=Some_Virtual_Host name=ignore_this_queue_as_well || exit 1
 echo success
